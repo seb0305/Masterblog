084295c38da2ecedc3f150fd868cdbe867ab3890
Sebastian Schnichels <seb0305@gmx.de>
2025-11-07 17:43:09 +0100

    Basic routes and data structure

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
+def load_posts():
+    json_path = os.path.join(app.static_folder, 'blog_posts.json')
+    with open(json_path, 'r') as f:
+        return json.load(f)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
