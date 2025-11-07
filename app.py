0e51bf743bfb95b39aab617decd24d7687b96741
Sebastian Schnichels <seb0305@gmx.de>
2025-11-07 17:52:41 +0100

    'Add' route added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import json
 
@@ -9,10 +9,37 @@ def load_posts():
     with open(json_path, 'r') as f:
         return json.load(f)
 
+def save_posts(posts):
+    json_path = os.path.join(app.static_folder, 'blog_posts.json')
+    with open(json_path, 'w') as f:
+        json.dump(posts, f, indent=4)
+
+
 @app.route('/')
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        posts = load_posts()
+
+        new_id = max(post['id'] for post in posts) + 1 if posts else 1
+        new_post = {
+            "id": new_id,
+            "author": request.form.get('author'),
+            "title": request.form.get('title'),
+            "content": request.form.get('content')
+        }
+
+        posts.append(new_post)
+        save_posts(posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
